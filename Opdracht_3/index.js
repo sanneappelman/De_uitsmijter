db79d5b1a4bad0b07b0d8538ef5d83324b163546
sanneappelman <84176938+sanneappelman@users.noreply.github.com>
2021-05-31 06:33:46 +0200

    Add files via upload

---
+++ Opdracht_3/index.js
@@ -0,0 +1,27 @@
+const age = 18;
+
+if(age == 18) {
+    console.log ("Je mag naar binnen");
+} else if(age >18) {
+    console.log ("Je mag naar binnen");
+} else if(age < 18) 
+    console.log ("helaas pindakaas");
+
+
+
+const isFemale = true;
+
+if(isFemale) {
+    console.log ("Je bent een vrouw");
+} else {
+    console.log ("Je bent geen vrouw, geen feest.");
+}
+
+const driverStatus = 'bob';
+
+if(driverStatus == 'bob') {
+    console.log ("jij bent de bob, je rijdt en drinkt niet")
+} else {
+    console.log ("jij drinkt en rijdt dus niet!")
+}
+
